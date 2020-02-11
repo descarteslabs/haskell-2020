5a40fd811f611d41968c99595bad866292f8133d
Matt Calef <mattcalef@descarteslabs.com>
2020-02-11 04:38:57 +0000

    Adding comments

--- scratch/matt/sample.hs
+++ scratch/matt/sample.hs
@@ -1,13 +1,17 @@
+-- The n-th prime number
 prime n = seive [2..] !! n
   where
     seive (x:xs) = x:[y | y <- (seive xs), y `mod` x /= 0]
 
+-- The n-th Fibonacci number
 fib n = fibs !! n
   where
     fibs = 1:1:zipWith (+) (drop 1 fibs) fibs
 
+-- n factorial
 fact n = foldl (*) 1 [1..n]
 
+-- sort a list
 sort [] = []
 sort [x] = [x]
 sort (x:xs) = [y | y <- sort lo] ++ [x] ++ [y | y <- sort hi]
